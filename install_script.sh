a69313293853f1f947859f622111ced6231d1601
Rob <robvangastel@hotmail.com>
2021-03-02 13:09:38 +0100

    Adjust installation script and experiment location

--- install_script.sh
+++ install_script.sh
@@ -1,5 +1,12 @@
+# pwd
+# ~/Git/meta-learners
 conda create --name meta python=3.7
 conda activate meta
+
+cd ..
+git clone https://github.com/rlworkgroup/garage.git
+cd garage
+
 
 # pip dependencies
 # pip install bsuite
@@ -9,9 +16,10 @@ pip install -e .['dev','all']
 pip install gym-bandits
 
 # conda dependencies
+# Care for GPU cuda should also be installed,
 Y | conda install pytorch torchvision torchaudio -c pytorch
-# Y | conda install -c conda-forge tensorflow
 Y | conda install -c conda-forge gym
 Y | conda install -c conda-forge jupyterlab
 # Y | conda install -c conda-forge matplotlib
 # Y | conda install -c conda-forge pandas
+# Y | conda install -c conda-forge tensorflow
